67f26b00a550ac119aa031e09ee400354c3452d0
Mohammed Abdul Abi <mohammedabdulabi@gmail.com>
2025-12-31 02:23:11 +0100

    Checks if user is logged in and displays go home button or not

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,17 +1,24 @@
-import { SignIn } from "@stackframe/stack";
+"use client";
+
+import { SignIn, useUser } from "@stackframe/stack";
 import Link from "next/link";
 
-export default function signInPage() {
+export default function SignInPage() {
+  const currentUser = useUser();
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-purple-50 to bg-purple-100">
-      <div className="max-w-md w-full space-y-8">
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-purple-50 to-purple-100">
+      <div className="max-w-md space-y-8">
         <SignIn />
-        <Link
-          href={"/"}
-          className="stack-scope inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-9 px-4 py-2 relative mt-6"
-        >
-          Go Home
-        </Link>
+
+        {!currentUser && (
+          <Link
+            href="/"
+            className="stack-scope inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-9 px-4 py-2 relative mt-6 w-full"
+          >
+            Go Home
+          </Link>
+        )}
       </div>
     </div>
   );
